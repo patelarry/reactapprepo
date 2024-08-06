b96817cbd89eb7a42bb715d3c71e46f0c701cf02
patelarry <aryanindoqubix@gmail.com>
2024-08-06 06:10:08 +0530

    Update validate_server.sh

--- scripts/validate_server.sh
+++ scripts/validate_server.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+# #!/bin/bash
 
-# for now we are just checking that we get back a page from the server
-echo validating server
-#curl -m 5 http://localhost:8080
+# # for now we are just checking that we get back a page from the server
+# echo validating server
+# #curl -m 5 http://localhost:8080
