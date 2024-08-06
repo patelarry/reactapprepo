8f3bd630b23a109373345847b0ec9852c7d4869b
patelarry <aryanindoqubix@gmail.com>
2024-08-06 06:03:27 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-echo starting server
 
-# here we just use npm to run the build
-cd /var/www/
-echo building application...
-sudo npm run build
+# Navigate to the project directory
+cd /home/ubuntu/server
 
-# start the application with pm2
-echo starting application...
-sudo pm2 start npm -- run production
+# Stop all running pm2 processes
+pm2 delete all
+
+# Start the application using pm2 with the npm start script
+pm2 start npm -- start
+
+
+
+
+
+
+
+# #!/bin/bash
+
+
+# # here we just use npm to run the build
+# cd /home/ubuntu/server
+
+
+# # start the application with pm2
+# pm2 delete all
+# pm2 start "npm start"
